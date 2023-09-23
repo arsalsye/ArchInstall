1c125e345b3369cd7588a96f63fb4a5eef598740
Arsalan Syed <arsalansyed96@outlook.com>
2023-09-23 16:34:09 +0000

    Git config

--- install_apps.sh
+++ install_apps.sh
@@ -2,13 +2,14 @@ sudo pacman -Syu
 
 sudo pacman -S alacritty bpytop dolphin firefox grub i3 neofetch\
 	networkmanager	nitrogen papirus-icon-theme polybar\
-       	python-pywal ranger rofi taskwarrior unzip\
+       	python-pywal ranger rofi task unzip\
        	xorg-server xorg-xinit
 
-ln -sv .xinitrc ~/.xinitrc
-ln -sv .taskrc ~/.taskrc
-ln -sv .bashrc ~/.bashrc
-ln -sv .bash_aliases ~/.bash_aliases
+ln -sv ~/ArchInstall/.xinitrc ~/.xinitrc
+ln -sv ~/ArchInstall/.taskrc ~/.taskrc
+ln -sv ~/ArchInstall/.bashrc ~/.bashrc
+ln -sv ~/ArchInstall/.bash_aliases ~/.bash_aliases
+ln -sv ~/ArchInstall/.gitconfig ~/.gitconfig
 ln -sv ~/ArchInstall/.config/alacritty/ ~/.config/alacritty
 ln -sv ~/ArchInstall/.config/i3/ ~/.config/i3
 ln -sv ~/ArchInstall/.config/nitrogen/ ~/.config/nitrogen
