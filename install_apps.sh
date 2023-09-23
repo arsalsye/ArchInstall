0a751ff74b47f0a981030388149342a57be2abb3
Arsalan Syed <arsalansyed96@outlook.com>
2023-09-23 15:58:35 +0000

    Some packages

---
+++ install_apps.sh
@@ -0,0 +1,8 @@
+sudo pacman -Syu
+
+sudo pacman -S alacritty bpytop dolphin firefox grub i3 neofetch\
+	networkmanager	nitrogen papirus-icon-theme polybar\
+       	python-pywal ranger rofi taskwarrior unzip\
+       	xorg-server xorg-xinit
+
+ln -sv .xinitrc ~/.xinitrc
