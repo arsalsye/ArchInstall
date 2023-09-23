dce7b47353b078218a0bcd917b74b8ded63563d6
Arsalan Syed <arsalansyed96@outlook.com>
2023-09-23 16:15:51 +0000

    Save config files

--- install_apps.sh
+++ install_apps.sh
@@ -6,3 +6,12 @@ sudo pacman -S alacritty bpytop dolphin firefox grub i3 neofetch\
        	xorg-server xorg-xinit
 
 ln -sv .xinitrc ~/.xinitrc
+ln -sv .taskrc ~/.taskrc
+ln -sv ~/ArchInstall/.config/alacritty/ ~/.config/alacritty
+ln -sv ~/ArchInstall/.config/i3/ ~/.config/i3
+ln -sv ~/ArchInstall/.config/nitrogen/ ~/.config/nitrogen
+ln -sv ~/ArchInstall/.config/picom/ ~/.config/picom
+ln -sv ~/ArchInstall/.config/polybar/ ~/.config/polybar
+ln -sv ~/ArchInstall/.config/ranger/ ~/.config/ranger
+ln -sv ~/ArchInstall/.config/rofi/ ~/.config/rofi
+
