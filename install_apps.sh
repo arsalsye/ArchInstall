6c9a1ce5b4a5852f5707f6b47e18d41f63fc1913
Arsalan Syed <arsalansyed96@outlook.com>
2023-09-23 17:34:50 +0000

    gvim package

--- install_apps.sh
+++ install_apps.sh
@@ -1,6 +1,6 @@
 sudo pacman -Syu
 
-sudo pacman -S alacritty bpytop dolphin firefox grub i3 neofetch\
+sudo pacman -S alacritty bpytop dolphin firefox grub gvim i3 neofetch\
 	networkmanager	nitrogen papirus-icon-theme polybar\
        	python-pywal ranger rofi task unzip\
        	xorg-server xorg-xinit
